541484223d871956cbe70de987291decde32cab7
Jonathan Kamens <jik@quantopian.com>
2017-08-17 10:08:58 -0400

    ToggleReplied 0.4 from Thunderbird
    This commit contains the unmodified files from version 0.4 of the
    ToggleReplied Thunderbird add-on, downloaded from
    https://addons.mozilla.org/thunderbird/downloads/latest/togglereplied-10558/addon-10558-latest.xpi,
    which is linked to from the add-on's home page,
    https://addons.mozilla.org/en-US/thunderbird/addon/togglereplied-10558/.
    
    The maintainer of the add-on has not released a new version since
    2013, the web site for the add-on no longer works, and the
    maintainer's email address bounces. I am preparing to fork the
    maintainer's code and release a new version with compatibility fixes.

---
+++ defaults/Preferences/pref.js
@@ -0,0 +1,3 @@
+pref("extensions.togglereplied@eyrich-net.org.description", "chrome://togglereplied/locale/togglereplied.properties");
+pref("extensions.togglereplied@eyrich-net.org.creator", "chrome://togglereplied/locale/togglereplied.properties");
+pref("extensions.togglereplied@eyrich-net.org.translator", "chrome://togglereplied/locale/togglereplied.properties");
